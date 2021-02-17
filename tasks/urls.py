27836293b33229330fb650c2f5e2289df1aa3417
direyes <diego.reyes@greensill.com>
2021-02-17 00:25:12 -0500

    add jwt authentication for users

--- tasks/urls.py
+++ tasks/urls.py
@@ -19,7 +19,13 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
 urlpatterns = [
     url(r'api/', include(('app.urls', 'api'), namespace='api')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
 ]
